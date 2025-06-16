6505cac96e8eb0daa161935c0b17faef304e6a01
Joonoor <zlata.ma9@gmail.com>
2025-06-16 15:26:19 +0300

    Добавлен урок 3 и практика

---
+++ lesson_03/lesson_3_task_1.py
@@ -0,0 +1,7 @@
+from user import User
+
+my_user = User("Zlata", "Sovik")
+
+my_user.say_first_name()
+my_user.say_last_name()
+my_user.say_info_user()
---
+++ lesson_03/lesson_3_task_3.py
@@ -0,0 +1,8 @@
+from mailing import Mailing
+from address import Address
+
+address_to = Address(169933, "Vorkuta", "Lenina", 36, 15)
+address_from = Address(175964, "Murmansk", "Polar", 9, 40)
+mailing = Mailing(address_to, address_from, "5000 рублей", 15654967)
+
+print(f"Отпрвление {mailing.track} из {address_from.index}, {address_from.city}, {address_from.street}, {address_from.home} - {address_from.flat} в {address_to.index}, {address_to.city}, {address_to.street}, {address_to.home} - {address_to.flat}. Стоимость {mailing.cost}.")
