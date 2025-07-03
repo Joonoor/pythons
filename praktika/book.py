0c1cf1159fd8ff1b437265054542e01a93a9666b
Joonoor <zlata.ma9@gmail.com>
2025-07-03 19:41:08 +0300

    Удалены дубли файлов

--- praktika/book.py
+++
@@ -1,4 +0,0 @@
-class Book:
-    def __init__(self, name, avtor):
-        self.name = name
-        self.avtor = avtor
--- praktika/student.py
+++
@@ -1,9 +0,0 @@
-class Student():
-    def __init__(self, fname, lname, age, cours):
-        self.fname = fname
-        self.lname = lname
-        self.age = age
-        self.cours = cours
-
-    def __str__(self):
-        return f"{self.fname} {self.lname}, {self.age} лет, курс: {self.cours}"
