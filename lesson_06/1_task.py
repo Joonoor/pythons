28a33eafe6304ff8dfd23d2c2144e6d0762a0305
Joonoor <zlata.ma9@gmail.com>
2025-07-26 14:34:39 +0300

    Исправление драйвера

--- lesson_06/1_task.py
+++ lesson_06/1_task.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(
+    service=ChromeService(ChromeDriverManager().install()))
 waiter = WebDriverWait(driver, 20)
 
 driver.get("http://uitestingplayground.com/ajax")
